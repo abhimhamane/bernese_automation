84768aa2f15a9dec391e5dee6c72caecbf8210fd
Abhishek Mhamane <abhi.mhamane30@gmail.com>
2024-04-23 22:53:38 +0530

    initial commit

---
+++ data_download_bernese.py
@@ -0,0 +1,11 @@
+# Script to automate downloading the required auxillary files like CRD, BRDC, Ephimeris, etc.. from appropriate FTP servers
+# Author: Abhishek Mhamane, MS-R Geoinformatics
+# Location: IIT Kanpur, India
+
+import os
+import sys
+import pathlib
+import bs4
+
+
+
---
+++ setup_new_proj.py
@@ -0,0 +1,10 @@
+#
+# Script to automate setting up processing daily GNSS data with Bernese
+# Author: Abhishek Mhamane, MS-R Geoinformatics
+# Location: IIT Kanpur, India
+
+import os
+import sys
+import pathlib
+
+
